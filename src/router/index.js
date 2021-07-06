9ca9b9da55ca6367d662b35e1472f3afcefaf694
chenyawei <781854554@qq.com>
2021-07-06 18:06:10 +0800

    开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import Router from "vue-router";
 
 // 懒加载导入组件
-const Home = () => import("../views/home/Home.vue");
-const Category = () => import("../views/category/Category.vue");
-const Shopcart = () => import("../views/cart/Shopcart.vue");
-const Profile = () => import("../views/profile/Profile.vue");
+const Home = () => import("@/views/home/Home.vue");
+const Category = () => import("@/views/category/Category.vue");
+const Shopcart = () => import("@/views/cart/Shopcart.vue");
+const Profile = () => import("@/views/profile/Profile.vue");
 
 Vue.use(Router);
 
