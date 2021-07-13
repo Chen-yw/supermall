f6d9de56852ee18d54da0fdc1fe1177ea844952a
chenyawei <781854554@qq.com>
2021-07-13 08:38:24 +0800

    .

--- src/network/detail.js
+++ src/network/detail.js
@@ -7,3 +7,41 @@ export function getDetail(iid) {
     }
   });
 }
+
+/* 由于在服务器拿到的数据不在一个地方，显得数据有点渣乱无章，所以可以将所有的数据保存到
+ * 一个对象内，在进行开发的时候，只需要面向一个对象进行开发就可以了。
+ */
+
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = services;
+    this.realPrice = itemInfo.lowNowPrice;
+  }
+}
+
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods;
+  }
+}
+
+/* class Person {
+  // ES5 创建一个Class类  constructor() 构造函数
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+// 创建类类Person的对象, 有两个属性，name和age
+const p = new Person("why", 18); */
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="detail">
-    <detail-nav-bar></detail-nav-bar>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <Scroll class="content">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar.vue";
-import { getDetail } from "network/detail.js";
+import DetailSwiper from "./childComps/DetailSwiper.vue";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+
+import Scroll from "components/common/scroll/Scroll";
+
+import { getDetail, Goods, Shop } from "network/detail.js";
+
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {}
     };
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll
   },
   created() {
     // console.log(this.$route.params);
@@ -25,11 +43,36 @@ export default {
 
     // 2.根据iid请求数据
     getDetail(this.iid).then(res => {
-      // console.log(res);
+      console.log(res);
+      const data = res.result;
       // 1.获取顶部的图片轮播数据
       this.topImages = res.result.itemInfo.topImages;
+
+      // 2.获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+
+      // 3.创建获取店铺信息对象
+      this.shop = new Shop(data.shopInfo);
     });
   }
 };
 </script>
-<style scoped></style>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.content {
+  height: calc(100% - 44px);
+}
+
+.detail-nav {
+}
+</style>
